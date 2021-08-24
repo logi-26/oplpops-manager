406750cf5fff4553abbafe59d7e278034a053898
louis <louis_gilmartin@hotmail.co.uk>
2021-08-24 10:55:35 +0100

    Initial commit

---
+++ src/build/start-oplpops.bat
@@ -0,0 +1,4 @@
+@echo off
+set root=/home/louis/Desktop/OPLPOPS-Manager/build
+CD /D %root%
+java -jar OPLPOPS-Manager_0.6.1.jar
---
+++ src/dist/start-oplpops.bat
@@ -0,0 +1,4 @@
+@echo off
+set root=/media/nas-raid/software/my software/POPS Game Manager/OPLPOPS-Manager/dist
+CD /D %root%
+java -jar OPLPOPS-Manager_0.6.1.jar
